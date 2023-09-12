3287369b409fe36de017096f33bcc11d931a58fe
MAC <mac@DERAs-MacBook-Pro.local>
2023-09-12 02:43:26 +0100

    hero banner widget

--- lib/widgets/hero_banner.dart
+++ lib/widgets/hero_banner.dart
@@ -5,6 +5,38 @@ class HeroBanner extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SliverToBoxAdapter(
+      child: SizedBox(
+        height: MediaQuery.of(context).size.height / 2.1,
+        child: Stack(
+          children: [
+            Image.network(
+              "https://firebasestorage.googleapis.com/v0/b/flutterbricks-public.appspot.com/o/airbnb%2Fairbnb_home.webp?alt=media&amp;token=27e4e303-eac2-4144-945f-73769bcb81f7&quot;",
+              fit: BoxFit.cover,
+              height: MediaQuery.of(context).size.height / 2.1,
+            ),
+            Align(
+              alignment: Alignment.center,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const SizedBox(
+                    height: 35,
+                  ),
+                  Text(
+                    "Not sure where to go?/nPerfect.",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+                  ),
+                  const SizedBox(
+                    height: 25,
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
