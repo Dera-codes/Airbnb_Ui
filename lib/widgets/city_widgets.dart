078b3305d220042592c3dc0fabb1b5aca91d00c8
Nnamdi Chidera Emmanuel <69612512+Dera-codes@users.noreply.github.com>
2023-09-13 06:42:42 +0100

    city widget

---
+++ lib/widgets/city_widgets.dart
@@ -0,0 +1,40 @@
+import 'package:airbnb_ui/models/city.dart';
+import 'package:flutter/material.dart';
+
+class CityCard extends StatelessWidget {
+  final City city;
+  const CityCard({required this.city, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(12.5),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(10),
+            child: Image.network(
+              city.image,
+              width: 59,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 5.0, top: 5.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(city.title,
+                    style:
+                        TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+                Text(city.subtitle,
+                    style:
+                        TextStyle(fontSize: 12, fontWeight: FontWeight.normal)),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
