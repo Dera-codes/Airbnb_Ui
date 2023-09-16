9216aee4411daa99b1ba7bc7be0b1cda0cc3a26b
Nnamdi Chidera Emmanuel <69612512+Dera-codes@users.noreply.github.com>
2023-09-16 20:16:40 +0100

    app update

--- lib/widgets/search_input.dart
+++ lib/widgets/search_input.dart
@@ -23,6 +23,17 @@ class SearchInput extends StatelessWidget {
                 const Icon(Icons.search, size: 20, color: Color(0xffFF5A60)),
             filled: true,
             fillColor: Colors.white,
+            hintText: 'Where are you going?',
+            hintStyle: TextStyle(color: Colors.black.withOpacity(.75)),
+            contentPadding:
+                const EdgeInsets.symmetric(vertical: 0.0, horizontal: 20.0),
+            border: const OutlineInputBorder(
+              borderRadius: BorderRadius.all(Radius.circular(15.0)),
+            ),
+            focusedBorder: const OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.white, width: 2.0),
+              borderRadius: BorderRadius.all(Radius.circular(15.0)),
+            ),
           ),
         ));
   }
